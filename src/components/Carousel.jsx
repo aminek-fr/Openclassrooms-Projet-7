77cd4a9655125e6771861f4eb5db4faa2412c888
Amine <keltoumi.amine@gmail.com>
2022-11-15 19:30:24 +0100

    carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,41 @@
+import { useState, useEffect } from 'react';
+import Vector from '../assets/Vector.svg'
 import '../sass/Layout/_carousel.scss'
-
+ 
 function Carousel ({pictures}){
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const length = pictures.length;
+    const nextSlide = () => {
+        setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1);
+      };
+    const prevSlide = () => {
+    setCurrentIndex(currentIndex === 0 ? length - 1 : currentIndex - 1);
+    };
+
     return(
-        <div className="carouselContainer">
-            {pictures.map((image, props) =>
-                <img key={props} className='carouselImg' src={image} alt='' />
-            )}
+        <div>
+            <img src={Vector} alt='' onClick={prevSlide} />
         </div>
+    {pictures.map((image, props) => {
+        return(
+        <div className="carouselContainer">
+            <div className='carouselItem'>
+                <img key={props} className='carouselImg' src={image} alt='' />
+            </div>
+        </div>
+        )
+    )}
+    }
     )
 }
 
 export default Carousel
+
+/*<div className="left-arrow">
+      onClick={prevSlide}
+      </div>
+    
+    <div className="right-arrow">
+      onClick={nextSlide}
+      </div>*/
 
--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -1,45 +1,27 @@
 import Carousel  from '../components/Carousel';
 import Products from '../data/data.json';
 import { useParams } from 'react-router-dom';
-//import '../sass/Layout/_carroussel.scss';
 import '../sass/Pages/_logements.scss';
 
 function Slider(){
     const id = useParams()
-    //console.log(id);
-    const log = Products.find(i => i.id === id.id);
-    //console.log(logement);
+    const oneProduct = Products.find(i => i.id === id.id);
 
-    const tagsLog = log?.tags.map((tags) => {
-        return <p>{tags}</p>
-    })
-    /*const imgLog = log?.pictures.map((image) => {
-        return <div>{image}</div>
-    })
-    console.log(imgLog);*/
-    
     return (
         <main className='mainContainer'>
-        <section className='sectionContainer'>
-        <div>{tagsLog}</div>
-        <Carousel pictures={log?.pictures} />
-        </section>
+            <section className='sectionContainer'>
+                <Carousel pictures={oneProduct?.pictures} />
+                <div>{oneProduct?.tags}</div>
+                
+            </section>
         </main>
     )
 }
 
 export default Slider
-/*{imgLog.map((product) => {
-    return (
-    <Carousel pictures={imgLog.pictures} />
-    <img src={product.pictures} alt='' />
-    )
-})
-{log?.pictures.map((product, k) => {
-        return (
-            //<Carousel pictures={product.pictures} />
-            <img key={k} src={product.pictures} alt='' />
-        )
-        })
-        }
-}*/
+
+/*
+const tagsLog = log?.tags.map((tags) => {
+        return <p>{tags}</p>
+    })
+<div>{tagsLog}</div>*/
