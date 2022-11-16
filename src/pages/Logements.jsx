100f13bab6ef6e77a6ac24f780ea40a615cebcf4
Amine <keltoumi.amine@gmail.com>
2022-11-16 16:14:29 +0100

    Carousel, Information page logement, étoiles et nom

--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -1,30 +1,61 @@
 import Carousel  from '../components/Carousel';
 import Products from '../data/data.json';
 import { useParams } from 'react-router-dom';
+import StarColors from '../assets/StarColors.svg'
+import StarWhite from '../assets/StarWhite.svg'
 import '../sass/Pages/_logements.scss';
 
 function Slider(){
     const id = useParams()
     const oneProduct = Products.find(i => i.id === id.id);
+    const tagsLog = oneProduct?.tags.map((tags) => {
+        return <p>{tags}</p>
+    })
+
+    //Note étoiles
+    let notLogement = []
+    let fullStar = true
+    for ( let index = 0; index < 5; index++) {
+        fullStar = false
+    if ( fullStar === true){
+        notLogement.push(<img key={index} className='etoiles' src={StarWhite} alt='' />) 
+    }else{
+        notLogement.push(
+            <img key={index} className='etoiles' src={StarColors} alt='' />
+        )
+    }
+    }
 
     return (
         <main className='mainContainer'>
             <section className='sectionContainer'>
                 <Carousel slides={oneProduct?.pictures} />
-                <div>{oneProduct?.tags}</div>
+                <div className='containerInformation'>
+                    <h1>{oneProduct?.title}</h1>
+                    <h2>{oneProduct?.location}</h2>
+                    <div className='containerTags'>
+                        <div className='tags'>{tagsLog}</div>
+                    </div>
+                </div>
+                <div className='containerStars'>
+                    <div>{notLogement}</div>
+                    <div className='containerName'>
+                        <div>{oneProduct?.host.name}</div>
+                        <div>{oneProduct.host.picture}</div>
+                    </div>
+                </div>
+                
             </section>
         </main>
     )
 }
 
 export default Slider
+/*<div>{oneProduct.host.picture}</div>*/
+/*<div>{oneProduct?.rating}</div>
+                <div>{oneProduct?.description}</div>
+                <div>{oneProduct?.equipments}</div>*/
 
-/*
-const tagsLog = log?.tags.map((tags) => {
-        return <p>{tags}</p>
-    })
-    const id = useParams()
-    const oneProduct = Products.find(i => i.id === id.id);
-    <img src={oneProduct?.pictures} alt='' />
-<div>{tagsLog}</div>*/
-
+                /*<div className='containerTags'>
+                    <div className='tags'>{tagsLog}</div>
+                </div>*/
