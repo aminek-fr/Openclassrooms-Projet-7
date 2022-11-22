7f81f58f866e870ae68349420845edb12be465ac
Keltoumi <keltoumi.amine@gmail.com>
2022-11-22 22:02:11 +0100

    Page About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,31 +1,30 @@
 import Dropdown from '../components/Dropdown';
-import Banner from '../assets/About.svg'
+import Banner from '../assets/About.png'
 import '../sass/Pages/_about.scss'
 
-function About() {
-    
+function About() { 
     return (
-        <div className='containerAbout'>
-            <img className='imgBanner' src={Banner} alt='' />
-            
-            <Dropdown title='Fiabilité' 
-            description='Les annonces postées sur Kasa garantissent une fiabilité totale. 
-            Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.' />
-            
-            <Dropdown title='Respect'
-            description='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire
-             ou de perturbation du voisinage entraînera une exclusion de notre plateforme.' />
-
-            <Dropdown title='Service'
-            description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. 
-            N'hésitez pas à nous contacter si vous avez la moindre question." />
-
-            <Dropdown title='Sécurité'
-            description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
-            chaque logement correspond aux critères de sécurité établis par nos services. 
-            En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
-            Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
-        </div>
+        <main className='mainAbout'>
+            <section className='sectionAbout'>
+                <div className='containerImg'>
+                    <img className='imageAbout' src={Banner} alt='' />
+                </div>
+                <Dropdown title='Fiabilité' 
+                description='Les annonces postées sur Kasa garantissent une fiabilité totale. 
+                Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.' />
+                <Dropdown title='Respect'
+                description='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire
+                ou de perturbation du voisinage entraînera une exclusion de notre plateforme.' />
+                <Dropdown title='Service'
+                description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. 
+                N'hésitez pas à nous contacter si vous avez la moindre question." />
+                <Dropdown title='Sécurité'
+                description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
+                chaque logement correspond aux critères de sécurité établis par nos services. 
+                En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. 
+                Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+            </section>
+        </main>
     )
 }
 
