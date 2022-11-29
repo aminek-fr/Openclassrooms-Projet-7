0b0964b6e4a30874edf1a1cebdaa8364b58c5c98
Amine <keltoumi.amine@gmail.com>
2022-11-29 18:02:42 +0100

    Correction gestion de l'erreur URL

--- src/pages/Logements.jsx
+++ src/pages/Logements.jsx
@@ -3,38 +3,43 @@ import Dropdown from '../components/Dropdown';
 import Rating from '../components/Rating';
 import Tags from '../components/Tags';
 import Products from '../data/data.json';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import '../styles/Pages/_logements.scss';
 
 function Slider(){
     const id = useParams()
     const oneProduct = Products.find(i => i.id === id.id);
-    
-    return (
-        <main className='mainContainerLogements'>
-            <section className='sectionContainerLogements'>
-                <Carousel slides={oneProduct.pictures} />
-                <article className='articleContainerLogements'>
-                    <div className='containerInformation'>
-                        <h1>{oneProduct.title}</h1>
-                        <h2>{oneProduct.location}</h2>
-                        <Tags tagsLodging={oneProduct.tags} />
-                    </div>
-                    <div className='containerStars'>
-                        <Rating rating={oneProduct.rating}/>
-                        <div className='containerName'>
-                            <p className='textName'>{oneProduct.host.name}</p>
-                            <img className='pictureName' src={oneProduct.host.picture} alt='' />
+
+    //Si l'id de l'url et différent de l'id présent dans le fichier "json" alors on renvoit sur la page d'erreur, sinon on affiche la page logement.//
+    if(!oneProduct){
+        return <Navigate to="*" />;
+    }else{
+        return (
+            <main className='mainContainerLogements'>
+                <section className='sectionContainerLogements'>
+                    <Carousel slides={oneProduct.pictures} />
+                    <article className='articleContainerLogements'>
+                        <div className='containerInformation'>
+                            <h1>{oneProduct.title}</h1>
+                            <h2>{oneProduct.location}</h2>
+                            <Tags tagsLodging={oneProduct.tags} />
                         </div>
-                    </div>
-                </article>
-                <article className='articleContainerDescription'>
-                    <Dropdown title="Description" description={oneProduct.description}/>
-                    <Dropdown title="Equipements" description={oneProduct.equipments}/>
-                </article>
-            </section>
-        </main>
-    )
+                        <div className='containerStars'>
+                            <Rating rating={oneProduct.rating}/>
+                            <div className='containerName'>
+                                <p className='textName'>{oneProduct.host.name}</p>
+                                <img className='pictureName' src={oneProduct.host.picture} alt='' />
+                            </div>
+                        </div>
+                    </article>
+                    <article className='articleContainerDescription'>
+                        <Dropdown title="Description" description={oneProduct.description}/>
+                        <Dropdown title="Equipements" description={oneProduct.equipments}/>
+                    </article>
+                </section>
+            </main>
+        )
+    }    
 }
 
 export default Slider
